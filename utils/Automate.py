521ccd81151403ee60b3548c12cde7c7cf4e4a0e
AbhishekTiwari07 <abhishekt2024@gmail.com>
2022-10-11 11:03:03 +0530

    Added Automate.py

---
+++ utils/Automate.py
@@ -0,0 +1,15 @@
+import RemoveChanges as rc
+import ModifyChanges as mc
+import Ediables
+import Nutrients
+
+def editFile(filename):
+    pass
+
+def createNutrientObject(**kwargs):
+    nutrient = Nutrient(kwargs)
+    return nutrient
+
+def createEdiableObject(**kwargs):
+    ediable = Ediables(kwargs)
+    return ediable 
